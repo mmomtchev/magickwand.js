4118831896f7786769d9fbd09890bdeb3b2ac47a
Momtchil Momtchev <momtchil@momtchev.com>
2023-04-29 15:31:22 +0200

    support arrays of drawables

--- test/Draw.test.js
+++ test/Draw.test.js
@@ -15,4 +15,16 @@ describe('Drawable', () => {
     const px = im.pixelColor(50, 50);
     assert.strictEqual(px.quantumBlue(), 65535);
   });
+
+  it('mutiple drawables per call', () => {
+    const im = new Image(new Geometry(100, 100), new Color('white'));
+
+    im.strokeColor(new Color('red'));
+    im.fillColor(new Color('blue'));
+    im.strokeWidth(2);
+
+    im.draw([new Magick.DrawableCircle(20, 50, 10, 50), new Magick.DrawableCircle(80, 50, 90, 50)]);
+    const px = im.pixelColor(20, 50);
+    assert.strictEqual(px.quantumBlue(), 65535);
+  });
 });
