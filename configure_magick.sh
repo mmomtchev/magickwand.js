2f24d9204a6893ccf1e64881d3f2541820b2fb4f
Momtchil Momtchev <momtchil@momtchev.com>
2023-05-03 21:56:39 +0200

    non-bash syntax

--- configure_magick.sh
+++ configure_magick.sh
@@ -2,8 +2,8 @@
 
 cd deps/ImageMagick
 CFLAGS="-fPIC"
-if [ $(uname) == 'Darwin' ]; then
-  CFLAGS="${CFLAGS} -I/usr/local/include"
-fi
+case $(uname) in
+  Darwin) CFLAGS="${CFLAGS} -I/usr/local/include" ;;
+esac 
 sh ./configure $2 --prefix=$1/ImageMagick --disable-shared --enable-static --disable-installed CFLAGS="${CFLAGS}" CXXFLAGS="${CFLAGS}" > /dev/null
 grep MAGICK_LIBS Makefile | cut -f 2 -d "="
