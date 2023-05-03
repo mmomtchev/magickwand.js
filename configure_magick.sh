5c64841da1fcac30c1c2527bc5b24778f12d0a13
Momtchil Momtchev <momtchil@momtchev.com>
2023-05-03 18:07:09 +0200

    do not build the shared library

--- configure_magick.sh
+++ configure_magick.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 cd deps/ImageMagick
-sh ./configure $1 --enable-static CFLAGS="-fPIC" CXXFLAGS="-fPIC" > /dev/null
+sh ./configure $1 --disable-shared --enable-static CFLAGS="-fPIC" CXXFLAGS="-fPIC" > /dev/null
 grep MAGICK_LIBS Makefile | cut -f 2 -d "="
