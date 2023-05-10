b8c51be29d2cd6f647908d8860afa5d2c89fa00e
Momtchil Momtchev <momtchil@momtchev.com>
2023-05-10 20:24:48 +0200

    remove the utilities

--- build_magick.sh
+++ build_magick.sh
@@ -10,7 +10,10 @@ export CXXFLAGS="-fPIC"
 export PKG_CONFIG_LIBDIR=$(pwd)/build
 
 cd deps/ImageMagick
-sh ./configure $2 --prefix=$1/ImageMagick --disable-openmp --disable-shared --enable-static --disable-installed
-make -j4
+
+# Do not include the utilities until they fix OpenJPEG on conan which requires deprecated glibc functions (finite-math)
+sh ./configure $2 --prefix=$1/ImageMagick --disable-openmp --disable-shared --enable-static --disable-installed --without-utilities
+
+make -j4 || true
 make install
 rm -rf $1/ImageMagick/lib/*.*a
