74c9a1a067a0831824f942bd4677652af152305e
Momtchil Momtchev <momtchil@momtchev.com>
2023-10-07 14:22:12 +0200

    add GetMagickFeatures

--- test/Core.test.ts
+++ test/Core.test.ts
@@ -4,9 +4,12 @@ import * as chai from 'chai';
 
 const assert = chai.assert;
 
-import * as IM from 'node-magickwand';
+import  * as IM from 'node-magickwand';
+import { MagickCore } from 'node-magickwand';
 
-console.log(`Built with ImageMagick ${IM.MagickLibVersionText}${IM.MagickLibAddendum} (${IM.MagickVersion})`);
+const features = MagickCore.GetMagickFeatures().trimEnd().split(' ').join(',');
+console.log(`Built with ImageMagick ${IM.MagickLibVersionText}${IM.MagickLibAddendum}` +
+  ` from ${MagickCore.GetMagickReleaseDate()} [${features}] (${IM.MagickVersion}) `);
 
 it('ImageMagick version information', () => {
   assert.isString(IM.MagickLibVersionText);
@@ -17,6 +20,13 @@ it('ImageMagick version information', () => {
   assert.strictEqual(IM.MagickQuantumRange, '65535');
   assert.strictEqual(IM.MagickHDRISupport, '-HDRI');
   assert.strictEqual(IM.NAPI_VERSION, 6);
+  assert.isString(MagickCore.GetMagickCopyright());
+  assert.isString(MagickCore.GetMagickDelegates());
+  assert.isString(MagickCore.GetMagickFeatures());
+  assert.isString(MagickCore.GetMagickHomeURL());
+  assert.isString(MagickCore.GetMagickLicense());
+  assert.isString(MagickCore.GetMagickPackageName());
+  assert.isString(MagickCore.GetMagickReleaseDate());
 });
 
 it('Security Policy', function () {
