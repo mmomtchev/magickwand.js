9e47758aca3c056872b2cdf5aa419180b58ed82e
Momtchil Momtchev <momtchil@momtchev.com>
2023-10-16 21:49:12 +0200

    use git to checkout

--- scripts/deps-download.js
+++ scripts/deps-download.js
@@ -1,5 +1,4 @@
 const cp = require('child_process');
-const https = require('https');
 const fs = require('fs');
 const path = require('path');
 const crypto = require('crypto');
@@ -32,53 +31,4 @@ try {
   process.exit(1);
 }
 
-async function download(url, targetFile) {
-  console.log('downloading', url, 'to', targetFile);
-  await fs.promises.mkdir(path.dirname(targetFile), { recursive: true });
-  await fs.promises.rm(targetFile, { force: true });
-  return new Promise((resolve, reject) => {
-    https.get(url, response => {
-      const code = response.statusCode ?? 0;
-
-      if (code >= 400) {
-        return reject(new Error(response.statusMessage));
-      }
-
-      // handle redirects
-      if (code > 300 && code < 400 && !!response.headers.location) {
-        return resolve(
-          download(response.headers.location, targetFile)
-        );
-      }
-
-      // save the file to disk
-      const fileWriter = fs
-        .createWriteStream(targetFile)
-        .on('finish', () => {
-          fileWriter.close();
-          resolve({});
-        });
-
-      response.pipe(fileWriter);
-    }).on('error', error => {
-      reject(error);
-    });
-  });
-}
-
-const q = [];
-for (const file of [
-  'magickcore.i',
-  'Magick++.cxx',
-  'magick++.i',
-  'magickwand.i',
-  'Magick++.d.ts',
-  'wasm/Magick++.cxx',
-  'wasm/Magick++.d.ts'
-]) {
-  q.push(download(
-    `https://raw.githubusercontent.com/mmomtchev/node-magickwand/${commit}/${file}`,
-    path.resolve(generated_path, file)
-  ));
-}
-Promise.all(q).then(() => console.log('done'));
+cp.execSync(`git --work-tree=${generated_path} checkout ${commit}`);
