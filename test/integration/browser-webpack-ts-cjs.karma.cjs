13dbcb4c5da566e50c2fa261b4f1a715cbab7f31
Momtchil Momtchev <momtchil@momtchev.com>
2023-10-18 11:59:50 +0200

    remove stray debug

--- test/integration/browser-webpack-ts-cjs.karma.cjs
+++ test/integration/browser-webpack-ts-cjs.karma.cjs
@@ -30,7 +30,7 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: false,
     browsers: ['Chrome'],
-    //singleRun: true,
+    singleRun: true,
     concurrency: Infinity
   });
 };
