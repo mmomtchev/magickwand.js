96bb071fc76745e50d6958379405db109ac61b1a
Momtchil Momtchev <momtchil@momtchev.com>
2023-10-23 14:31:15 +0200

    fix the ES6 import

--- wasm/index.js
+++ wasm/index.js
@@ -2,6 +2,15 @@ import * as emnapi from '@emnapi/runtime';
 import IM_import from '../lib/binding/emscripten-wasm32/magickwand.js';
 
 const binding = IM_import()
-  .then((IM_emnapi) => IM_emnapi.emnapiInit({ context: emnapi.getDefaultContext() }));
+  .then((IM_emnapi) => {
+    const mod = IM_emnapi.emnapiInit({ context: emnapi.getDefaultContext() });
+    Object.defineProperty(mod, 'FS', {
+      value: IM_emnapi.FS,
+      enumerable: true,
+      configurable: false,
+      writable: false
+    });
+    return mod;
+  });
 
 export default binding;
