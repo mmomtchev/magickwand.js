b673e74f4b03247b7e4aa404fd7a1eabe37a0b14
Momtchil Momtchev <momtchil@momtchev.com>
2023-10-24 11:47:41 +0200

    shave one `js` off the file names (#60)
    * these are not needed anymore
    
    * shave off one .js from the file names

--- src/pre.js
+++
@@ -1,7 +0,0 @@
-// This is the empscripten preamble
-// It is needed to load the JS worker when running in a WebWorker
-
-var Module = {
-  ...(Module || {}),
-  'mainScriptUrlOrBlob': 'magickwand.js.js'
-};
