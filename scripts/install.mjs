83e61292723c1e73fd01ad29b0ee7c6cdda1551d
Momtchil Momtchev <momtchil@momtchev.com>
2023-10-25 19:13:54 +0200

    download the WASM binaries even if build-from-source is passed

--- scripts/install.mjs
+++ scripts/install.mjs
@@ -6,7 +6,6 @@ let native = false;
 let wasm = false;
 
 const cmd = os.platform() === 'win32' ? 'npx.cmd' : 'npx';
-
 if (!process.env.npm_config_build_from_source) {
   console.log(chalk.cyan(`Trying to install prebuilt native binaries for ${os.platform()}-${os.arch()}...`));
   try {
@@ -32,7 +31,10 @@ if (!native || process.env.npm_config_build_from_source) {
 
 console.log(chalk.cyan('Trying to install WASM binaries for emscripten-wasm32...'));
 try {
-  cp.execFileSync(cmd, ['node-pre-gyp', 'install', '--target_platform=emscripten', '-target_arch=wasm32']);
+  cp.execFileSync(cmd,
+    ['node-pre-gyp', 'install', '--target_platform=emscripten', '--target_arch=wasm32'],
+    { env: { ...process.env, npm_config_build_from_source: undefined } }
+  );
   wasm = true;
 } catch (e) {
   console.log(chalk.yellow('Failed...'));
