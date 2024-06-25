6250a1fa8700c403cf341ff74430bcfe606671b2
Momtchil Momtchev <momtchil@momtchev.com>
2024-06-25 13:59:47 +0200

    fix more cases

--- test/integration/browser-webpack-ts-cjs/index.ts
+++ test/integration/browser-webpack-ts-cjs/index.ts
@@ -4,7 +4,19 @@ import chaiAsPromised from 'chai-as-promised';
 chai.use(chaiAsPromised);
 const assert = chai.assert;
 
-import IM from 'magickwand.js';
+/** 
+ * TS transpiled to CJS cannot use the automatic import (it requires ES6).
+ * If you transpile to CJS, you will have to manually select the
+ * right version - Node.js native or browser WASM.
+ * 
+ * Also, in the particular case of browser TypeScript transpiled to
+ * CJS, you will have to switch your module resolution to node16.
+ *
+ * Consider migrating to ES6, in 2024 the tools have evolved and most
+ * of the problems of the early days have been solved. TS transpiled to
+ * ES6 offers numerous advantages for both browser and Node.js code.
+ */
+import IM from 'magickwand.js/wasm';
 
 describe('Image', () => {
   it('ImageMagick version information', () =>
--- test/integration/node-ts-esm/index.ts
+++ test/integration/node-ts-esm/index.ts
@@ -1,7 +1,7 @@
 import { assert } from 'chai';
 import ImageMagick from 'magickwand.js';
 
-it('TS (transpiled to CJS) import test', (done) => {
+it('TS (transpiled to ES6) import test', (done) => {
   ImageMagick.then(({ Magick }) => {
     const im = new Magick.Image;
     assert.instanceOf(im, Magick.Image);
