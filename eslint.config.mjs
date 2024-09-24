a7ecf34c69c2cde52d15ba48d820ccd4638a43c9
Momtchil Momtchev <momtchil@momtchev.com>
2024-09-24 18:01:33 +0200

    simplify the config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,11 @@
-import mocha from "eslint-plugin-mocha";
-import globals from "globals";
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
-import tsParser from "@typescript-eslint/parser";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import mocha from 'eslint-plugin-mocha';
+import globals from 'globals';
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -15,87 +15,68 @@ const compat = new FlatCompat({
     allConfig: js.configs.all
 });
 
-export default [...compat.extends("eslint:recommended"), {
-    plugins: {
-        mocha,
-    },
-
-    languageOptions: {
-        globals: {
-            ...globals.mocha,
-            ...globals.node,
+export default [
+    ...compat.extends('eslint:recommended'), {
+        plugins: {
+            mocha,
         },
 
-        ecmaVersion: 2020,
-        sourceType: "commonjs",
-    },
+        languageOptions: {
+            globals: {
+                ...globals.mocha,
+                ...globals.node,
+            },
 
-    rules: {
-        semi: [2, "always"],
-        quotes: ["error", "single"],
-    },
-}, ...compat.extends(
-    "eslint:recommended",
-    "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended",
-).map(config => ({
-    ...config,
-    files: ["test/*.ts"],
-})), {
-    files: ["test/*.ts"],
-
-    plugins: {
-        "@typescript-eslint": typescriptEslint,
-    },
-
-    languageOptions: {
-        parser: tsParser,
-    },
-
-    rules: {
-        "@typescript-eslint/ban-ts-comment": "off",
-    },
-}, {
-    files: ["**/*.mjs", "lib/*.js", "wasm/*.js"],
-
-    languageOptions: {
-        ecmaVersion: 2022,
-        sourceType: "module",
-    },
-}, {
-    files: ["test/integration/browser*/*.js", "example/browser/**/*.js"],
-
-    languageOptions: {
-        globals: {
-            ...globals.browser,
+            ecmaVersion: 2020,
+            sourceType: 'commonjs',
         },
 
-        ecmaVersion: 2020,
-        sourceType: "module",
+        rules: {
+            semi: [2, 'always'],
+            quotes: ['error', 'single'],
+        },
     },
-}, ...compat.extends(
-    "eslint:recommended",
-    "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended",
-).map(config => ({
-    ...config,
-    files: ["test/integration/browser*/*.ts"],
-})), {
-    files: ["test/integration/browser*/*.ts"],
+    ...compat.extends(
+        'eslint:recommended',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+    ).map(config => ({
+        ...config,
+        files: ['test/*.ts'],
+    })),
+    {
+        files: ['test/*.ts'],
 
-    plugins: {
-        "@typescript-eslint": typescriptEslint,
-    },
-
-    languageOptions: {
-        globals: {
-            ...globals.browser,
+        plugins: {
+            '@typescript-eslint': typescriptEslint,
         },
 
-        parser: tsParser,
-    },
+        languageOptions: {
+            parser: tsParser,
+        },
 
-    rules: {
-        "@typescript-eslint/ban-ts-comment": "off",
+        rules: {
+            '@typescript-eslint/ban-ts-comment': 'off',
+        },
     },
-}];
+    {
+        files: ['**/*.mjs', 'lib/*.js', 'wasm/*.js'],
+
+        languageOptions: {
+            ecmaVersion: 2022,
+            sourceType: 'module',
+        },
+    },
+    {
+        files: ['test/integration/browser*/*.js', 'example/browser/**/*.js'],
+
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+            },
+
+            ecmaVersion: 2020,
+            sourceType: 'module',
+        },
+    }
+];
