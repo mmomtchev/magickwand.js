7c5d601890668bc13161628299c5e6de89abb088
Momtchil Momtchev <momtchil@momtchev.com>
2025-02-06 23:57:34 +0100

    use ChromeHeadles and drop xvfb

--- test/integration/browser-webpack-ts-esm.karma.cjs
+++ test/integration/browser-webpack-ts-esm.karma.cjs
@@ -29,7 +29,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: false,
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadless'],
     singleRun: true,
     concurrency: Infinity
   });
